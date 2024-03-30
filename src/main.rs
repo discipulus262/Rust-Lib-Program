8bad2c517975f938ed482207899249018f9596ff
Live session user <nico@webmacd.com>
2024-03-30 18:52:09 +0000

    start the project

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::collections::HashMap;
+
+struct Book {
+    title: String,
+    author: String,
+}
+struct DVD {
+    title: String,
+}
 fn main() {
-    println!("this works!");
+    let mut data = HashMap::new();
+    data.insert("Chronicles of Narnia".to_string(), "C.S. Lewis".to_string());
 }
